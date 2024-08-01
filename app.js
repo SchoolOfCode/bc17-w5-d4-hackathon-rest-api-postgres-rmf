af6d69a220838bdd96b221d635c02f86dee77386
Marco Di Summa <mdisumma@yahoo.com>
2024-08-01 17:52:24 +0100

    setting database

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 // Import the required modules
 import express from "express";
 
-
-
 // Import your helper functions for your first resource here
 // import {
 //   getResourceOne,
@@ -11,7 +9,6 @@ import express from "express";
 //   updateResourceOneById,
 //   deleteResourceOneById,
 // } from "./resource_one.js";
-
 
 // Import your helper functions for your second resource here
 // import {
@@ -22,8 +19,6 @@ import express from "express";
 //   deleteResourceTwoById,
 // } from "./resource_two.js";
 
-
-
 // Initialize the express app
 const app = express();
 // Retrieve the port number from environment variables
@@ -31,63 +26,45 @@ const PORT = process.env.PORT;
 
 app.use(express.json()); // express.json() middleware is used to parse incoming JSON requests
 
-
-
-
 // Resource One Route Handlers
 
 // Endpoint to retrieve all <resource_one>
-app.get("/resourceone/", async function (req, res) {
-    console.log("I'm alive");
-    res.status(200).send("I'm alive!");
+app.get("/heros/", async function (req, res) {
+  console.log("I'm alive");
+  res.status(200).send("I'm alive!");
 });
 
 // Endpoint to retrieve a <resource_one> by id
-app.get("/resourceone/:id", async function (req, res) {
-});
+app.get("/hero/:id", async function (req, res) {});
 
 // Endpoint to create a new <resource_one>
-app.post("/resourceone/", async function (req, res) {
-});
+app.post("/hero/", async function (req, res) {});
 
 // Endpoint to update a specific <resource_one> by id
-app.patch("/resourceone/:id", async function (req, res) {
-});
+app.patch("/hero:id", async function (req, res) {});
 
 // Endpoint to delete a specific <resource_one> by id
-app.delete("/resourceone/:id", async function (req, res) {
-});
-
-
-
+app.delete("/hero/:id", async function (req, res) {});
 
 // Resource Two Route Handlers
 
 // Endpoint to retrieve all <resource_twos>
-app.get("/resourcetwo/", async function (req, res) {
-    const authors = await getAuthors();
-    res.status(200).json({ status: "success", data: authors });
-  });
-  
-  // Endpoint to retrieve a <resource_twos> by id
-  app.get("/resourcetwo/:id", async function (req, res) {
-  });
-  
-  // Endpoint to create a new <resource_twos>
-  app.post("/resourcetwo/", async function (req, res) {
-  });
-  
-  // Endpoint to update a specific <resource_twos> by id
-  app.patch("/resourcetwo/:id", async function (req, res) {
-  });
-  
-  // Endpoint to delete a specific <resource_twos> by id
-  app.delete("/resourcetwo/:id", async function (req, res) {
-  });
+app.get("/creator/", async function (req, res) {
+  const authors = await getAuthors();
+  res.status(200).json({ status: "success", data: authors });
+});
 
+// Endpoint to retrieve a <resource_twos> by id
+app.get("/creator/:id", async function (req, res) {});
 
+// Endpoint to create a new <resource_twos>
+app.post("/creator/", async function (req, res) {});
 
+// Endpoint to update a specific <resource_twos> by id
+app.patch("/creator/:id", async function (req, res) {});
 
+// Endpoint to delete a specific <resource_twos> by id
+app.delete("/creator/:id", async function (req, res) {});
 
 // Start the server and listen on the specified port
 app.listen(PORT, function () {
