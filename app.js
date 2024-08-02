bdf653caeee49c3314c4abafc2c18e26d74a11f0
Marco Di Summa <mdisumma@yahoo.com>
2024-08-02 11:42:49 +0100

    working on createHero

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ import express from "express";
 
 // Import your helper functions for your first resource here
 import {
-getHeroes,
-getHeroById,
-createHero,
-//   updateResourceOneById,
-//   deleteResourceOneById,
+  getHeroes,
+  getHeroById,
+  createHero,
+  //   updateResourceOneById,
+  //   deleteResourceOneById,
 } from "./hero.js";
 
 // Import your helper functions for your second resource here
@@ -30,40 +30,40 @@ app.use(express.json()); // express.json() middleware is used to parse incoming 
 
 // Endpoint to retrieve all <hero>
 app.get("/hero/", async function (req, res) {
-try {
-  const result = await getHeroes()
-  res.status (200).json({status: true, data: result})
-}
-  catch (error) {
-  res.status (404).json({status: 'Error: 404. Heros not found', data: error})
+  try {
+    const result = await getHeroes();
+    res.status(200).json({ status: true, data: result });
+  } catch (error) {
+    res
+      .status(404)
+      .json({ status: "Error: 404. Heros not found", data: error });
   }
 });
-
 
 // Endpoint to retrieve a <hero> by id
 app.get("/hero/:id", async function (req, res) {
   try {
-  const data = req.params.id;
-  const result = await getHeroById(data)
-  res.status (200).json ({status: 'success', data: result})
-  } catch (error){
-    res.status (404).json({status: 'Error: 404. Id not found', data: error})
+    const data = req.params.id;
+    const result = await getHeroById(data);
+    res.status(200).json({ status: "success", data: result });
+  } catch (error) {
+    res.status(404).json({ status: "Error: 404. Id not found", data: error });
   }
-})
+});
 
 // Endpoint to create a new <hero>
 app.post("/hero/", async function (req, res) {
-    const data = req.body;
-    console.log(data)    
-    res.json(data)    
-    const result = await createHero(data.hero_name, data.real_name, data.first_appearance)
-    res.status (200).json ({status: 'success', data: result})
-
-
-    //await createHero(body)
-
+  const data = req.body;
+  console.log("from postman", data);
+  res.json(data);
+  const result = await createHero(
+    data.hero_name,
+    data.real_name,
+    data.first_appearance
+  );
+  res.status(200).json({ status: "success", data: result });
 });
- 
+
 // Endpoint to update a specific <resource_one> by id
 app.patch("/hero/:id", async function (req, res) {});
 
@@ -77,7 +77,6 @@ app.get("/resourcetwo/", async function (req, res) {
   const authors = await getAuthors();
   res.status(200).json({ status: "success", data: authors });
 });
-
 
 // Start the server and listen on the specified port
 app.listen(PORT, function () {
--- hero.js
+++ hero.js
@@ -3,34 +3,34 @@ import { pool } from "./db/index.js";
 
 // Query the database and return all heroes
 export async function getHeroes() {
-  // Define the SQL query to fetch all heroes from the 'heroes' table  
+  // Define the SQL query to fetch all heroes from the 'heroes' table
   const text = "SELECT * FROM heroes";
-  const result = await pool.query(text)
+  const result = await pool.query(text);
   // Use the pool object to send the query to the database
   // return the result
-  return result.rows
+  return result.rows;
 }
 
 // Query the database and return the hero with a matching id or null
 export async function getHeroById(id) {
   // Define the SQL query to fetch hero corrisponding to the request.params.id from 'heroes' table
-  const text= "SELECT * FROM heroes WHERE id = $1"
-  const result = await pool.query(text,[id])
+  const text = "SELECT * FROM heroes WHERE id = $1";
+  const result = await pool.query(text, [id]);
   // Use the pool object to send the query to the database
   // return the result or null
-  return result.rows[0] || null
+  return result.rows[0] || null;
 }
 
 // Query the database to create an hero and return the newly created hero
 export async function createHero(hero_name, real_name, first_appearance) {
   // Define the SQL query to fetch an hero corrisponding to the request.dody and add it to 'heroes' table
   const text = `INSERT INTO heroes (hero_name, real_name, first_appearance)
-                VALUES ($1, $2, $3) RETURNING *`
-                [hero_name, real_name, first_appearance];
+                VALUES ($1, $2, $3) RETURNING *`[
+    (hero_name, real_name, first_appearance)
+  ];
   const result = await pool.query(text);
-  console.log(result)
-  return result.rows || null
-
+  console.log("from handler", result);
+  return result;
 
   // Use the pool object to send the query to the database
   // return the result or null
